82b13e7cd633bce1ee587505becfa168648affa8
Jérôme Desmares <jdesmare@student.42.fr>
2017-12-10 14:33:50 +0100

    Ingredients are ready to be checked

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ var express = require('express');
 var app = express();
 var path = require('path')
 var data = require('./data.json');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(8080);
 console.log("Please see: http://localhost:8080/");
 
 app.get('/data', function(req, res) {
-        console.log(data);
         res.json(data);
 });
 app.post('/cook', function(req, res) {
-  console.log(req);
+  console.log(req.body);
   res.sendfile('./public/index.html');
   //fs.writeFile('lol.json', JSON.stringify(new_data));
 });
